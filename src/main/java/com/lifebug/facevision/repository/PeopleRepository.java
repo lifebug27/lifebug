928beb26522df91dc7651d502145c180748bdd00
Ivanekrasov <ivan.and.nekrasov@gmail.com>
2018-10-27 17:28:53 +0300

    initial

--- src/main/java/com/lifebug/facevision/repository/PeopleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lifebug.facevision.repository;
-
-import com.lifebug.facevision.model.People;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PeopleRepository extends CrudRepository<People, Integer> {
-}
