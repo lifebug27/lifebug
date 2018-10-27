2f7deb4abd49894e52b88a50d11a41c7ea4247e8
Nick_Jv <rnjv2015@yandex.ru>
2018-10-27 19:58:58 +0300

    add field status in People.class

--- src/main/java/com/lifebug/facevision/model/People.java
+++ src/main/java/com/lifebug/facevision/model/People.java
@@ -14,12 +14,12 @@ public class People {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "status")
+    private String status;
+
     public People() {
     }
 
-    public People(String name) {
-        this.name = name;
-    }
 
     public Integer getId() {
         return id;
@@ -37,17 +37,27 @@ public class People {
         this.name = name;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         People people = (People) o;
         return Objects.equals(id, people.id) &&
-                Objects.equals(name, people.name);
+                Objects.equals(name, people.name) &&
+                Objects.equals(status, people.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+
+        return Objects.hash(id, name, status);
     }
 }
