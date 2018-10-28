a5166bd88dd51d5a6a3e9066525cd235cc40580d
Ivanekrasov <ivan.and.nekrasov@gmail.com>
2018-10-28 10:13:00 +0300

    fix bootstrap

---
+++ target/classes/static/js/main.js
@@ -0,0 +1,87 @@
+var peopleApi = Vue.resource('/api/people{/id}');
+
+Vue.component('person-form', {
+    props: ['people'],
+    data: function () {
+        return {
+            text: ''
+        }
+    },
+    template:
+    '<div>' +
+    '<input type="text" placeholder="Write something" v-model="text" />' +
+    '<input type="button" value="Save" v-on:click="save" />' +
+    '</div>',
+    methods: {
+        save: function () {
+            let person = {text: this.name, status: this.status};
+
+            peopleApi.save({}, person)
+                .then(result =>
+                result.json()
+                    .then(data => {
+                    this.people.push(data);
+                    this.name = '';
+                    this.status = '';
+                })
+            )
+        }
+    }
+});
+
+Vue.component('person-row', {
+    props: ['person'],
+    template: '<div>' +
+    '<i>({{ person.id }})</i> ' +
+        '{{person.name}} ' +
+        '-------->' +
+        '{{person.status}} ' +
+    // '<span>' +
+    // '<input type="button" value="Edit" v-on:click="edit" />' +
+    // '</span>' +
+    '</div>',
+    methods: {
+        edit: function () {
+
+        }
+    }
+});
+
+Vue.component('people-list', {
+    props: ['people'],
+    data: function () {
+        return{
+
+        }
+    },
+    template:
+    '<div>' +
+    '<person-form :people="people" />' +
+    '<person-row v-for = "person in people" :key="person.id" :person="person">' +
+    '{{person.name}}' +
+    '</person-row>' +
+    '</div>',
+    created: function () {
+        peopleApi.get()
+            .then(result => result.json()
+                .then(data =>
+                    data.forEach(person =>
+                        this.people.push(person)
+                )
+            )
+        )
+    },
+    methods: {
+        editMessage: function () {
+            this.person = person;
+        }
+    }
+});
+
+var app = new Vue({
+    el: '#app',
+    template: '<people-list :people="people" />',
+    data: {
+        people: []
+    }
+});
---
+++ target/classes/static/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <header>
+            <h1>Vue.js SPA</h1>
+        </header>
+        <main>
+            <aside class="sidebar">
+            </aside>
+            <div class="content">
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
