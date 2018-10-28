02bbefff8fdd94842558671cb13f72ce1709bf01
NekoEater <cimacha@yandex.ru>
2018-10-28 03:39:36 +0300

    fix data sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,39 +30,3 @@ VALUES ('28.10.2018', 'Hack ends', '13:00');
 
 INSERT INTO darsad67es2t64.public.event (date, event_name, event_time)
 VALUES ('28.10.2018', 'Hack breakfast', '8:00');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (1, 1, 'true');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (1, 2, 'true');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (1, 3, 'false');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (1, 4, 'true');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (2, 1, 'true');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (2, 2, 'true');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (2, 3, 'false');
-
-INSERT INTO darsad67es2t64.public.attendance(event_id, par_id, status)
-VALUES (2, 4, 'true');
-
-INSERT INTO people(name, status)
-VALUES (Nekrasov, 'no');
-
-INSERT INTO people(name, status)
-VALUES (Cha, 'no');
-
-INSERT INTO people(name, status)
-VALUES (Van, 'no');
-
-INSERT INTO people(name, status)
-VALUES (Kolya, 'no');
